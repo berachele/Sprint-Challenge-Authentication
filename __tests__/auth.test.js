0c9f2beccab6eaaaf7c3e3564eb7e82911a45ceb
Rachele Edwards <dot.com1472@gmail.com>
2020-05-22 09:50:29 -0600

    setup tests folder, test script, test layout and beforeEach. First test is passing

---
+++ __tests__/auth.test.js
@@ -0,0 +1,32 @@
+const supertest = require('supertest')
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+
+beforeEach(() => {
+    return db.migrate.rollback()
+    .then(()=>db.migrate.latest())
+    // .then(()=>db.seed.run())
+})
+
+describe('AUTH', () => {
+    it('can run the test', () => {
+        expect(true).toBeTruthy()
+    })
+
+    describe('/REGISTER', () => {
+        it('should add a new user, 201 status', () => {
+            return supertest(server)
+            .post('/api/auth/register')
+            .send({username: "Mary_Poppins", password: "Supercalifragilistic"})
+            .then(response => {
+                expect(response.status).toEqual(201)
+            })
+        })
+        it.todo('should return that user (as an array of 1)')
+    })
+
+    describe('/LOGIN', ()=> {
+        it.todo('can login successfully, 200 status')
+        it.todo('can see the `Welcome, <username>` string')
+    })
+})
